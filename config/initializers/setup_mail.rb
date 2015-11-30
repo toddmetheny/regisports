cec7417090dbafd2e6b50831a4f487c7c95f29d9
Antonio Santoni <antonio@antoniosantoni.com>
2015-11-29 23:43:10 -0500

    Fix Typo

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
 	:port				=>	'587',
 	:authentication		=>	:plain,
 	:user_name			=>	ENV[SENDGRID_USERNAME],
-	:password			=>	ENV[SENDGRID_USERNAME],
+	:password			=>	ENV[SENDGRID_PASSWORD],
 	:domain				=>	'heroku.com',
 	:eneble_starttls_auto => true
 }

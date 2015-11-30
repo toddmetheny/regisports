fcd22184084a3f95ce09e247afe68231c1dd2dd1
Antonio Santoni <antonio@antoniosantoni.com>
2015-11-29 19:40:20 -0500

    Mailer setup heroku domain

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'regisports.herokuapp.com',
+  :domain => 'regisports2.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,

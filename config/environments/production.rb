477d15da770e3f6573b0fda627a7eb06fdfebf56
Antonio Santoni <antonio@antoniosantoni.com>
2015-11-29 19:46:38 -0500

    Mailer config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,9 @@ Rails.application.configure do
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
-config.action_mailer.default_url_options = { :host => 'regisport2.heroku.com' }
+Rails.application.routes.default_url_options[:host] = 'regisport2.herokuapp.com'
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
 end

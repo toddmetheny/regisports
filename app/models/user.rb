57fc166c0f9e6a8f75c495b0e536f7bb0d90a837
Antonio Santoni <antonio@antoniosantoni.com>
2015-12-14 21:06:28 -0500

    Removed Geocode validation from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
-	geocoded_by :current_sign_in_ip,
-	  :latitude => :lat, :longitude => :lon
-	after_validation :geocode         
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable   
 
   belongs_to :role
 
   def admin?
   	self.role.name == 'admin'
-  end
-
-  
+  end  
 end

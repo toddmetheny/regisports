1195a42b808a75a203f7ea82ab7b3a719475c36d
Antonio Santoni <antonio@antoniosantoni.com>
2016-01-09 11:31:51 -0500

    Added current_order method to Application controller, added cart text partial to layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+protect_from_forgery with: :exception	
 before_action :configure_permitted_parameters, if: :devise_controller?
+helper_method :current_order
 
 protected
 
@@ -7,8 +11,13 @@ def configure_permitted_parameters
   devise_parameter_sanitizer.for(:sign_up) << :name
   devise_parameter_sanitizer.for(:account_update) << :name
 end
-	
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+def current_order
+  if !session[:order_id].nil?
+    Order.find_by_uuid(session[:order_id])
+  else
+    Order.new
+   end
+end	
+
 end

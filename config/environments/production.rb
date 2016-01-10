10dbf48ce1a9f556d96003342e7917825ed0583d
Antonio Santoni <antonio@antoniosantoni.com>
2016-01-10 10:13:41 -0500

    Changed SENDGRID URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   }
 }
 
- Rails.application.routes.default_url_options[:host] = 'regisport2.herokuapp.com'
+ Rails.application.routes.default_url_options[:host] = 'https://regisportv01.herokuapp.com'
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.perform_deliveries = true
  config.action_mailer.raise_delivery_errors = false

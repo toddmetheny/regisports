77e7f8c93ba83c922c75a2c0e4b126ca6d5ba949
Antonio Santoni <antonio@antoniosantoni.com>
2016-01-09 23:07:09 -0500

    Uncommneted  remove order_id_id

--- db/migrate/20160110025447_remove_order_id_id_from_reservations.rb
+++ db/migrate/20160110025447_remove_order_id_id_from_reservations.rb
@@ -1,5 +1,5 @@
 class RemoveOrderIdIdFromReservations < ActiveRecord::Migration
   def change
-#    remove_column :reservations, :order_id_id, :string
+    remove_column :reservations, :order_id_id, :string
   end
 end

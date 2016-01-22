cbe5c5bfeee0066ac95b2092b6397b026b7fdc81
Antonio Santoni <antonio@antoniosantoni.com>
2016-01-21 23:12:52 -0500

    Improving look and feel and added main.js for GASP animation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,23 @@
+
+customAnimation = {
+
+	init: function()
+	{
+
+		console.log("foo")
+
+
+	} // end init
+
+
+}; //end cuntomAnimation
+
+
+
+
 $( document ).ready(function() {
 
-
+	customAnimation.init();
 
 }); // end ready function
 
